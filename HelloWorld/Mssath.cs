9d7eed1216e5f2a072e20a3bcb1a6fdcc6726b29
Ted K <ted.k07@gmail.com>
2023-02-03 22:32:44 +0100

    Add project files.

---
+++ HelloWorld/Mssath.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    internal class Mssath
+    {
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Substract(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double Multiplication(double a, double b)
+        {
+            return a * b;
+        }
+
+
+
+
+
+    }
+}
---
+++ HelloWorld/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace HelloWorld
+{
+    // PascalCase
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 20;
+            double b = 10;
+
+           Mssath math = new Mssath();
+
+            double addResult = math.Add(a, b);
+            double substractResult = math.Substract(a, b);
+            double multiplicationResult = math.Multiplication(a, b);
+
+            Console.WriteLine(addResult.ToString());
+            Console.WriteLine(substractResult);
+            Console.WriteLine(multiplicationResult);
+
+
+            //// camelCase
+            //string imie = "Tadeusz";
+
+            ////UPPER_CASE
+            //const double PI_NUMBER = 3.14;
+
+            //Console.WriteLine("Hello, World! {0}", PI_NUMBER);
+            //Console.WriteLine("Nazywam się Tadeusz Kobroń, {0}", imie);
+
+
+
+
+
+         }
+
+
+    }
+
+}
+
+
+
